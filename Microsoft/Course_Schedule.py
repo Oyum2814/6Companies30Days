1b481936c443528a882d01814bb1ad886039f687
Om <ommukherjee28@gmail.com>
2023-01-04 01:28:16 +0530

    Q7 | Runtime (193 ms) Beats 52.69% | Memory (15.5 MB) Beats 87.81%

--- Microsoft/Course_Schedule.py
+++ Microsoft/Course_Schedule.py
@@ -1,22 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
         graph = [[] for _ in range(numCourses)]
         in_degree = [0 for _ in range(numCourses)]
         for pair in prerequisites:
             x, y = pair
             graph[y].append(x)
             in_degree[x] += 1
+            
         queue = []
         for i in range(numCourses):
             if in_degree[i] == 0:
                 queue.append(i)
-        while queue:
+                
+        while queue: 
             curr = queue.pop(0)
             for neighbor in graph[curr]:
                 in_degree[neighbor] -= 1
                 if in_degree[neighbor] == 0:
                     queue.append(neighbor)
+                    
         for i in range(numCourses):
             if in_degree[i] > 0:
                 return False
+            
         return True
